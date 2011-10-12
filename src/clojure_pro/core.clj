d08144625352a46e90b9249b609f8616afa343c8
Ramamohan Chokkam <rchokkam@gmail.com>
2011-10-12 14:22:07 +0200

    added lein project

---
+++ src/clojure_pro/core.clj
@@ -0,0 +1,1 @@
+(ns clojure-pro.core)
---
+++ test/clojure_pro/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-pro.test.core
+  (:use [clojure-pro.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
