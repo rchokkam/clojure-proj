1f8358ac17de11241f4cff9b6476c7b830f7cc5f
Ramamohan Chokkam <rchokkam@gmail.com>
2011-10-12 14:35:08 +0200

    fix the commit issue

--- project.clj
+++ project.clj
@@ -1,3 +1,9 @@
-(defproject clojure-pro "1.0.0-SNAPSHOT"
+(defproject test-project "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]])
+
+(defproject test-project "0.1.0"
+  :description "Test Project"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.3.2"]])
